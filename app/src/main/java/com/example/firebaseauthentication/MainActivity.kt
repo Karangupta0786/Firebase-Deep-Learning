32aea594985109745dd7b94135fb61576cecd0eb
Karan <karansuggu@gmail.com>
2024-04-24 19:33:06 +0530

    Firebase authentication initialise.

--- app/src/main/java/com/example/firebaseauthentication/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauthentication/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.firebaseauthentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.firebaseauthentication.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        auth.useAppLanguage()
+
+        binding.btnOtp.setOnClickListener {
+            val phone = "+91"+binding.etNumber.text.toString()
+            getOtp(phone)
+        }
+
+    }
+
+    private fun getOtp(phoneNumber:String) {
+
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                    Toast.makeText(applicationContext, "completed", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    Toast.makeText(applicationContext, "failed!!", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(p0, p1)
+
+                }
+
+            }) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
 }
